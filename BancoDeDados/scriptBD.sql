a0fb40e49f7a596e7d86fe45c8392c2b21541811
carlosOsilva <carlos.osilva@sptech.school>
2023-11-06 10:54:47 -0300

    Script do Banco de Dados Atualizado

--- BancoDeDados/scriptBD.sql
+++ BancoDeDados/scriptBD.sql
@@ -7,28 +7,29 @@ idEmpresa int primary key auto_increment,
 email varchar(30),
 senha varchar(20),
 cnpj char(14),
-telefone char(11)
+telefoneFixo char(11),
+telefoneCelular char(12)
 );
 
 create table endereco(
 idEndereco int primary key auto_increment,
 estado varchar(15),
 cep char(8),
-endereco varchar(40),
+logradouroo varchar(40),
 numero varchar(4),
 cidade varchar(30),
-complemento varchar(30),
-fkEmpresa int,
-constraint fkE foreign key (fkEmpresa)
-references empresa(idEmpresa)
+complemento varchar(30)
 );
 
 create table galpao(
 idGalpao int primary key auto_increment,
 nomeGalpao varchar(30),
+tipoDeEucalipto varchar(45),
 fkEmpresa int, 
-constraint fkEmp foreign key (fkEmpresa)
-references empresa(idEmpresa));
+foreign key (fkEmpresa) references empresa(idEmpresa),
+fkEndereco int,
+foreign key (fkEndereco) references endereco(idEndereco)
+);
 
 create table sensores(
 idSensores int primary key auto_increment,
@@ -37,10 +38,7 @@ fkGalpao int,
 constraint fkG foreign key(fkGalpao)
 references galpao(idGalpao)
 );
-insert into sensores values
-(1, 'Umidade', null);
 
-select * from sensores;
 
 create table registros
 (idRegistros int auto_increment,
@@ -52,11 +50,6 @@ references sensores(idSensores),
 primary key(idRegistros,fkSensor)
 );
 
-select * from registros;
-
-select * from endereco as e join empresa as emp on e.fkEmpresa= emp.idEmpresa 
-join galpao as g on emp.idEmpresa = g.fkEmpresa 
-join sensores as s on s.fkGalpao=g.idGalpao join registros as r on r.fkSensor=s.idSensores;
 
 
 
