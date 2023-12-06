8ca1bb585e7f540fb0c56feccc3d3ea8c69ffa6d
RyanMirandaBarbosa <ryan.barbosa@sptech.school>
2023-12-06 00:05:09 -0300

    tentando resolver bug - não carrega as outras páginas e da erro

--- Site/src/routes/index.js
+++ Site/src/routes/index.js
@@ -3,7 +3,7 @@ var path = require("path");  // Adicione esta linha para importar o módulo path
 var router = express.Router("");
 
 router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../public/html/sprint3/projeto.html"));
+    res.sendFile(path.join(__dirname, "../../public/html/projeto.html"));
 });
 
 module.exports = router;
