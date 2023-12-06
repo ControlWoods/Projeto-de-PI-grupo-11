cbba79bff69c1045ae9f05478fedf742c7db99b0
RyanMirandaBarbosa <ryan.barbosa@sptech.school>
2023-12-05 21:23:39 -0300

    começando back end

---
+++ Site/src/routes/index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var path = require("path");  // Adicione esta linha para importar o módulo path
+var router = express.Router("");
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/html/sprint3/projeto.html"));
+});
+
+module.exports = router;
