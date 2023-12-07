3d708c979f30fb4e3238f6305f9ec8524ff32f7c
RyanMirandaBarbosa <ryan.barbosa@sptech.school>
2023-12-07 15:27:43 -0300

    atualizando modelagem lógica e scrip BD

--- BancoDeDados/scriptBD.sql
+++ BancoDeDados/scriptBD.sql
@@ -1,51 +1,59 @@
 create database controlWoods;
 use controlWoods;
 
-
 create table empresa(
 idEmpresa int primary key auto_increment,
-email varchar(30) not null,
-senha varchar(20) not null,
+nome varchar(45) not null,
+email varchar(45) not null,
+senha varchar(45) not null,
 cnpj char(14) not null,
-telefoneFixo char(11),
-telefoneCelular char(12)
+telefoneFixo char(10),
+telefoneCelular char(11)
 );
 
-create table endereco(
-idEndereco int primary key auto_increment,
-estado varchar(15) not null,
-cep char(8) not null,
-logradouro varchar(40) not null,
-numero varchar(4),
-cidade varchar(30) not null,
-complemento varchar(30)
-);
-
-create table galpao(
+create table galpao (
 idGalpao int primary key auto_increment,
-nomeGalpao varchar(30),
-tipoDeEucalipto varchar(45) not null,
+nome varchar(45),
 fkEmpresa int not null, 
-foreign key (fkEmpresa) references empresa(idEmpresa),
-fkEndereco int not null,
-foreign key (fkEndereco) references endereco(idEndereco)
+foreign key (fkEmpresa) references empresa(idEmpresa)
 );
 
 create table sensores(
-idSensores int primary key auto_increment,
-tipo varchar(20) not null,
-fkGalpao int not null,
-constraint fkG foreign key(fkGalpao)
-references galpao(idGalpao)
+idSensor int primary key auto_increment,
+fkGalpao int,
+fkEmpresa int,
+constraint fkG foreign key(fkGalpao) references galpao(idGalpao),
+constraint fkE foreign key (fkEmpresa) references empresa(idEmpresa)
 );
 
-
-create table registros
-(idRegistros int auto_increment,
+create table registros (
+idRegistro int auto_increment,
 umidade float not null,
-horario datetime not null,
+dtHorario datetime not null,
 fkSensor int,
-constraint fkS foreign key (fkSensor)
-references sensores(idSensores),
-primary key(idRegistros,fkSensor)
+constraint fkS foreign key (fkSensor) references sensores(idSensor),
+primary key(idRegistro, fkSensor)
 );
+
+select * from empresa;
+insert into empresa values 
+(null, 'teste', "teste", "oooii",1223333,070079797,80808808);
+
+select * from galpao; 
+
+insert into galpao values 
+(null, 'galp1', 1);
+
+select * from sensores;
+
+insert into sensores values
+(null, 1, 1);
+
+select * from registros;
+insert into registros values
+(null, 32.0, "2020-04-12 12:00:00", 1);
+
+select * from empresa join galpao on idEmpresa=fkEmpresa
+join sensores on fkGalpao=idGalpao join registros on idRegistro = fkSensor;
+
+select registros.umidade, registros.dtHorario from registros join sensores on fkSensor = idSensor where fkGalpao = 1;
